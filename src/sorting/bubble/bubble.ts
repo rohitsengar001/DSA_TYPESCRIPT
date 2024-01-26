d7fed3bf87e061cba1762cded63954b4b84574c3
rohitsengar8923 <rohitsengar@consultadapt.com>
2024-01-27 01:28:19 +0530

    feat: :zap: improve bubble sort

--- src/sorting/bubble/bubble.ts
+++ src/sorting/bubble/bubble.ts
@@ -1,15 +1,18 @@
 function bubbleSort(arr: Array<number>) {
+  let flag = false
   for (let i = 1; i <= arr.length - 1; i++) {
-    for (let j = 0; j < arr.length - i; j++) {
+    for (let j = 0; j < arr.length - i ; j++) {
       if (arr[j] > arr[j + 1]) {
         //swap the elements
         const temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        flag = true
       }
     }
+    if(!flag) break
   }
   return arr
 }
 
-console.log(bubbleSort([2, 4, 5, 6, 1]));
+console.log(bubbleSort([2, 4,7, 5, 6, 1]));
