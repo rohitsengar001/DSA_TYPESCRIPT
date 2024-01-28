02ae67fb88e561eb6499775fdca9b057e593f540
rohitsengar8923 <rohitsengar@consultadapt.com>
2024-01-28 23:47:14 +0530

    feat: :sparkles: Heap Sort

---
+++ src/sorting/heap/heap.ts
@@ -0,0 +1,38 @@
+function heapSort(arr: Array<number>) {
+  buildMaxHeap(arr);
+  for (let i = arr.length - 1; i > 0; i--) {
+    //Exchange Root(Maximum Number) Last Element(lowest Number) Inside Heap
+    [arr[i], arr[0]] = [arr[0], arr[i]];
+
+    //heapify the heap again
+    //due to lowest elmenent comes at root element
+    maxHeapify(arr, i, 0);
+  }
+  return arr;
+}
+
+function buildMaxHeap(arr: Array<number>) {
+  for (let i = Math.floor(arr.length / 1); i > -1; i--) {
+    maxHeapify(arr, arr.length, 0);
+  }
+}
+
+function maxHeapify(arr: Array<number>, heapSize: number, rootIndex: number) {
+  let largest = rootIndex;
+  const left = 2 * rootIndex + 1;
+  const right = 2 * rootIndex + 2;
+
+  //compare the left and right for largest alongwith rootIndex
+  if (left < heapSize && arr[left] > arr[largest]) largest = left;
+  if (right < heapSize && arr[right] > arr[largest]) largest = right;
+
+  if (largest !== rootIndex) {
+    // swap root with largest element
+    [arr[largest], arr[rootIndex]] = [arr[rootIndex], arr[largest]];
+
+    //recurisevely Heapify the sub-tree
+    maxHeapify(arr, heapSize, largest);
+  }
+}
+
+console.log(heapSort([4, 10, 3,-2, 5, 1]));
---
+++ src/sorting/heap/readme.md
@@ -0,0 +1,22 @@
+# HeapSort
+- it's using the Binary Tree Data-Stucture [MAX/MIN HEAP]
+- A max heap is a binary tree where every node has a value greater than or equal to its children and its vice-versa is called MIN Heap.  
+- the children of a node at index i are located at indices 2i+1 and 2i+2.
+- left children smaller than right children (in ref. of value)
+
+  ![Alt text](image.png)
+
+## BUILD MAX-HEAP
+- Complexity to build MAX-HEAP - O(n)
+
+  ![Alt text](image-1.png)
+
+## MAX HEAPIFY
+- Time Complexity : log n
+![Alt text](image-2.png)
+
+- Pictorial Representation
+![img](https://www.codesdope.com/staticroot/images/algorithm/heapsort1.gif)
+
+## Time Complexity
+ ![img](https://big-o.io/assets/heapsort.png)
